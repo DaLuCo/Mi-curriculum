8eee0b74c90b9d61433363fbfb6556c7975172ed
DaLuCo <108887880+DaLuCo@users.noreply.github.com>
2023-02-20 18:40:16 -0300

    Cambios 11

--- Mi-CV/src/app/app.module.ts
+++ Mi-CV/src/app/app.module.ts
@@ -12,6 +12,7 @@ import { ExperienciaComponent } from './components/experiencia/experiencia.compo
 import { EducacionComponent } from './components/educacion/educacion.component';
 import { NgCircleProgressModule } from 'ng-circle-progress';
 import { HardYSoftComponent } from './components/hard-y-soft/hard-y-soft.component';
+import { ProyectoComponent } from './components/proyecto/proyecto.component';
 
 @NgModule({
   declarations: [
@@ -23,7 +24,8 @@ import { HardYSoftComponent } from './components/hard-y-soft/hard-y-soft.compone
     AcercaDeComponent,
     ExperienciaComponent,
     EducacionComponent,
-    HardYSoftComponent
+    HardYSoftComponent,
+    ProyectoComponent
   ],
   imports: [
     BrowserModule,
