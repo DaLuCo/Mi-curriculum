615f5378b09f6ed6946980b9906079cf85c86edd
DaLuCo <108887880+DaLuCo@users.noreply.github.com>
2023-02-20 16:56:32 -0300

    Cambios 11

---
+++ Mi-CV/src/app/components/hard-y-soft/hard-y-soft.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hard-y-soft',
+  templateUrl: './hard-y-soft.component.html',
+  styleUrls: ['./hard-y-soft.component.css']
+})
+export class HardYSoftComponent {
+
+}
