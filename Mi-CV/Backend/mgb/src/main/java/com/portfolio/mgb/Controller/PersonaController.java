c52019e4fd83f85230afb6e3d197ef9dbde06690
danie <daniela.comes@outlook.com>
2023-03-15 17:02:00 -0300

    Cambios 26

--- Mi-CV/Backend/mgb/src/main/java/com/portfolio/mgb/Controller/PersonaController.java
+++ Mi-CV/Backend/mgb/src/main/java/com/portfolio/mgb/Controller/PersonaController.java
@@ -17,32 +17,31 @@ import org.springframework.web.bind.annotation.RestController;
 public class PersonaController {
     @Autowired IPersonaService ipersonaService;
     
-    @GetMapping("personas/traer")
+    @GetMapping("/personas/traer")
     public List<Persona> getPersona()
     {return ipersonaService.getPersona();}
     
     @PostMapping("/personas/crear")
     public String createPersona(@RequestBody Persona persona)
-       {ipersonaService.savePersona(persona);
-        return "La persona fue creada correctamente";}
+        {ipersonaService.savePersona(persona);
+         return "La persona fue creada correctamente";}
     
     @DeleteMapping("/personas/borrar/{id}")
     public String deletePersona(@PathVariable Long id)
-    {ipersonaService.deletePersona(id);
-     return "La persona fue eliminada correctamente";}
+        {ipersonaService.deletePersona(id);
+         return "La persona fue eliminada correctamente";}
     
     @PutMapping("/personas/editar/{id}")
-    public Persona editPersona
-        (@PathVariable Long id,
-         @RequestParam("nombre") String nuevoNombre,
-         @RequestParam("apellido") String nuevoApellido,
-         @RequestParam("img") String nuevoImg)                
-    {Persona persona = ipersonaService.findPersona(id);
-    
-     persona.setNombre(nuevoNombre);
-     persona.setApellido(nuevoApellido);
-     persona.setImg(nuevoImg);
-    
-    ipersonaService.savePersona(persona);
-    return persona;}
-}
+    public Persona editPersona(@PathVariable Long id,
+                               @RequestParam("nombre") String nuevoNombre,
+                               @RequestParam("apellido") String nuevoApellido,
+                               @RequestParam("img") String nuevoImg)
+        {Persona persona = ipersonaService.findPersona(id);
+         
+         persona.setNombre(nuevoNombre);
+         persona.setApellido(nuevoApellido);
+         persona.setImg(nuevoImg);
+        
+         ipersonaService.savePersona(persona);
+         return persona;}
+    }
--- Mi-CV/Backend/mgb/src/main/java/com/portfolio/mgb/Service/ImpPersonaService.java
+++ Mi-CV/Backend/mgb/src/main/java/com/portfolio/mgb/Service/ImpPersonaService.java
@@ -12,7 +12,7 @@ public class ImpPersonaService implements IPersonaService{
     @Autowired IPersonaRepository ipersonaRepository;
 
     @Override
-    public List<Persona> getPersona()
+    public List<Persona> getPersona() 
         {List<Persona> persona = ipersonaRepository.findAll();
         return persona;}
 
@@ -27,5 +27,6 @@ public class ImpPersonaService implements IPersonaService{
     @Override
     public Persona findPersona(Long id)
         {Persona persona = ipersonaRepository.findById(id).orElse(null);
-         return persona;}    
+         return persona;}
+    
 }
