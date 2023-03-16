e086cdf3473ef38f42c65194bad1fdc152d186dd
danie <daniela.comes@outlook.com>
2023-03-16 16:56:03 -0300

    Update persona.service.ts

--- Mi-CV/Frontend/src/app/service/persona.service.ts
+++ Mi-CV/Frontend/src/app/service/persona.service.ts
@@ -10,5 +10,5 @@ export class PersonaService {
   URL = 'http://localhost:8080/personas/';
   constructor(private http: HttpClient) { }
   public getPersona(): Observable<persona>
-  {return this.http.get<persona>(this.URL + 'traer');}
+  {return this.http.get<persona>(this.URL + 'traer/perfil');}
 }
