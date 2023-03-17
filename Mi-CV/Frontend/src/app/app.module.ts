4e07d67a3bce02b559a605fcfa56fa41b82643d1
danie <daniela.comes@outlook.com>
2023-03-17 16:01:40 -0300

    Cambios 28

--- Mi-CV/Frontend/src/app/app.module.ts
+++ Mi-CV/Frontend/src/app/app.module.ts
@@ -13,7 +13,7 @@ import { NgCircleProgressModule } from 'ng-circle-progress';
 import { HardYSoftComponent } from './components/hard-y-soft/hard-y-soft.component';
 import { ProyectoComponent } from './components/proyecto/proyecto.component';
 import { FooterComponent } from './components/footer/footer.component';
-import {HttpClient, HttpClientModule } from '@angular/common/http';
+import {HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
--- Mi-CV/Frontend/src/app/components/acerca-de/acerca-de.component.ts
+++ Mi-CV/Frontend/src/app/components/acerca-de/acerca-de.component.ts
@@ -10,7 +10,7 @@ import { PersonaService } from 'src/app/service/persona.service';
 export class AcercaDeComponent implements OnInit{
   persona: persona = new persona("","","");
 
-  constructor(public personaService: PersonaService){}
+  constructor(public personaService: PersonaService){ }
 
   ngOnInit(): void
     {this.personaService.getPersona().subscribe(data => {this.persona = data})}
